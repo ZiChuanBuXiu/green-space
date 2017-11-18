0d3286ed7b743f381b89acd7345916bd1e55588d
yungang <yungangf@student.unimelb.edu.au>
2017-11-18 21:51:13 +1100

    add RankMenu

---
+++ src/components/RankMenu.js
@@ -0,0 +1,55 @@
+import {Divider, Drawer, List, ListItem, Subheader} from "material-ui";
+import {yellow500, cyan300} from "material-ui/styles/colors";
+
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import React, {Component} from 'react';
+
+const style = {
+    display: 'inline-block',
+    margin: '16px 32px 16px 0',
+};
+
+const ranksToText = {
+    1: "First choice:",
+    2: "Second choice:",
+    3: "Third choice:"
+};
+
+class RankMenu extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <Drawer
+                    zDepth={4}
+                    open={this.props.isDrawerOpen}
+                    style={style}
+                    containerStyle={{height: "60%", top: "100px", margin: "15px"}}>
+                    <List>
+                        <Subheader style={{backgroundColor: cyan300}}>Recommendations:</Subheader>
+                        <Divider/>
+                        {
+                            this.props.ranks.map(function (rank, i) {
+                                return (
+                                    <div>
+                                        <ListItem
+                                            leftIcon={<ActionGrade color={yellow500}/>}
+                                            secondaryTextLines={2}
+                                            primaryText={ranksToText[i + 1]}
+                                            secondaryText={rank}
+                                        />
+                                        <Divider/>
+                                    </div>
+                                )
+                            })}
+                    </List>
+                </Drawer>
+            </div>
+        );
+    }
+}
+
+export default RankMenu;
