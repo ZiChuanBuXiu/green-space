0cf200e0b032a43d2ae9426b63cd2d75c40b9ff8
yungang <yungangf@student.unimelb.edu.au>
2017-11-26 23:16:02 +1100

    add park clouds

--- src/components/InformationDialog.js
+++ src/components/InformationDialog.js
@@ -4,6 +4,18 @@ import {Card, CardActions, CardHeader, CardMedia, CardText, CardTitle, Dialog, R
 
 const image = require('../images/wordcloud-CarltonNorth.jpg');
 
+const parks = require('../data/parks.json');
+
+let parkWordCloudImages = {};
+
+for (let park in parks){
+    if (parks.hasOwnProperty(park)){
+        parkWordCloudImages[park] = require(`../images/wordclouds/${park}.png`);
+    }
+}
+
+console.log(parkWordCloudImages);
+
 class InformationDialog extends Component {
     handleClose = () => {
         this.props.onUpdate({dialogOpen: false})
@@ -31,7 +43,7 @@ class InformationDialog extends Component {
                         <CardMedia
                             overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle"/>}
                         >
-                            <img src={image} alt="" height={"100vh"}/>
+                            <img src={require('../images/wordcloud-CarltonNorth.jpg')} alt="" height={"100vh"}/>
                         </CardMedia>
                         <CardTitle title="Card title" subtitle="Card subtitle"/>
                         <CardText>
