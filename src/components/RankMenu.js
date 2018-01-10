e5faa114d7491b573e7add9d1e4bbf7ccd5a63c0
yungang <yungangf@student.unimelb.edu.au>
2018-01-10 15:07:11 +1100

    add pop and pol

--- src/components/RankMenu.js
+++ src/components/RankMenu.js
@@ -1,52 +1,82 @@
-import {Divider, Drawer, List, ListItem, Subheader} from "material-ui";
-import {yellow500, cyan300} from "material-ui/styles/colors";
+import {Divider, Drawer, List, ListItem, LinearProgress, Subheader} from 'material-ui';
+import {yellow500, cyan300} from 'material-ui/styles/colors';
 
 import ActionGrade from 'material-ui/svg-icons/action/grade';
 import React, {Component} from 'react';
 
 const style = {
-    display: 'inline-block',
-    margin: '16px 32px 16px 0',
+  display: 'inline-block',
+  margin: '16px 32px 16px 0',
 };
 
 const ranksToText = {
-    1: "First choice:",
-    2: "Second choice:",
-    3: "Third choice:"
+  1: 'First choice:',
+  2: 'Second choice:',
+  3: 'Third choice:'
 };
 
 class RankMenu extends Component {
-    render() {
-        let self = this;
-        return (
-            <Drawer
-                zDepth={4}
-                open={this.props.isDrawerOpen}
-                style={style}
-                containerStyle={{height: "60%", top: "100px", margin: "15px"}}>
-                <List>
-                    <Subheader style={{backgroundColor: cyan300}}>Recommendations:</Subheader>
-                    <Divider/>
-                    {
-                        this.props.searchResults.map(function (result, i) {
-                            return (
-                                <div>
-                                    <ListItem
-                                        leftIcon={<ActionGrade color={yellow500}/>}
-                                        primaryText={ranksToText[i + 1]}
-                                        secondaryText={result.name}
-                                        onClick={() => {
-                                            self.props.onUpdate({center: result.coordinates});
-                                        }}
-                                    />
-                                    <Divider/>
-                                </div>
-                            )
-                        })}
-                </List>
-            </Drawer>
-        );
-    }
+  render() {
+    let self = this;
+    return (
+      <Drawer
+        zDepth={4}
+        open={this.props.isDrawerOpen}
+        style={style}
+        containerStyle={{height: '60%', top: '100px', margin: '15px'}}>
+        <List>
+          <Subheader style={{backgroundColor: cyan300}}>Recommendations:</Subheader>
+          <Divider/>
+          {
+            this.props.searchResults.map(function (result, i) {
+              return (
+                <div>
+                  <ListItem
+                    leftIcon={<ActionGrade color={yellow500}/>}
+                    style={{height: '100px'}}
+                    onClick={() => {
+                      self.props.onUpdate({center: result.coordinates});
+                    }}
+                  >
+                    <h2></h2>
+                    <div marginHeight={'5px'}>
+                      <p style={{marginTop: 5, marginBottom: 5}}>Polarity:</p>
+                      <div>
+                        <LinearProgress
+                          min={0}
+                          max={100} mode={'determinate'}
+                          value={60}/>
+                      </div>
+                      <p style={{marginTop: 5, marginBottom: 5}}>Popularity:</p>
+                      <LinearProgress
+                        min={0}
+                        max={100} mode={'determinate'}
+                        value={60}/>
+                    </div>
+                    {/*<div width={'100%'} marginHeight={'15px'}>*/}
+                    {/*<div style={{float: 'left', width: '50%'}}><text fontSize={"20px"}>Popularity:</text></div>*/}
+                    {/*<div style={{float: 'right', width: '50%'}}><LinearProgress*/}
+                    {/*style={{width: '90%', align: 'center'}} min={0}*/}
+                    {/*max={100} mode={'determinate'}*/}
+                    {/*value={60}/></div>*/}
+                    {/*</div>*/}
+                    {/*<br/>*/}
+                    {/*<div width={'100%'} marginHeight={'5px'}>*/}
+                    {/*<div style={{float: 'left', width: '50%'}}><text fontSize={"10px"}>Polarity:</text></div>*/}
+                    {/*<div style={{float: 'right', width: '50%'}}><LinearProgress*/}
+                    {/*min={0}*/}
+                    {/*max={100} mode={'determinate'}*/}
+                    {/*value={60}/></div>*/}
+                    {/*</div>*/}
+                  </ListItem>
+                  <Divider/>
+                </div>
+              )
+            })}
+        </List>
+      </Drawer>
+    );
+  }
 }
 
 export default RankMenu;
